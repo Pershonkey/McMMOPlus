riking/mcMMO

Add diminished returns when a player has earned too much XP
When a player reaches a certain (configurable) threshold value of total
XP earned in a specific skill, his earned XP in this skill will be
decreased.

Depending on how far the player has exceeded the threshold value, his
XP will decrease more. After the (configurable) time interval of 10
minutes, the registered data will be cleared and the player can earn XP
as normal again.

src/main/java/com/gmail/nossr50/listeners/SelfListener.java
@@ -6,7 +6,11 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.gmail.nossr50.config.Config;
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
+import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
+import com.gmail.nossr50.events.experience.McMMOPlayerXpGainEvent;
+import com.gmail.nossr50.util.player.UserManager;
 import com.gmail.nossr50.util.skills.ParticleEffectUtils;
 
 public class SelfListener implements Listener {
@@ -28,5 +32,36 @@ public class SelfListener implements Listener {
         if ((skillValue % tier) == 0) {
             ParticleEffectUtils.runescapeModeCelebration(player, event.getSkill());
         }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerXpGain(McMMOPlayerXpGainEvent event) {
+        int threshold = Config.getInstance().getExperienceDeminishedReturnsThreshold();
+
+        if (threshold <= 0) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        McMMOPlayer mcMMOPlayer = UserManager.getPlayer(player);
+        SkillType skillType = event.getSkill();
+
+        if (skillType.isChildSkill()) {
+            return;
+        }
+
+        float difference = (mcMMOPlayer.getProfile().getRegisteredXpGain(skillType) - threshold) / threshold;
+
+        if (difference > 0) {
+//            System.out.println("Total XP Earned: " + mcMMOPlayer.getProfile().getRegisteredXpGain(skillType) + " / Threshold value: " + threshold);
+//            System.out.println(difference * 100 + "% over the threshold!");
+//            System.out.println("Previous: " + event.getRawXpGained());
+//            System.out.println("Adjusted XP " + (event.getRawXpGained() - (event.getRawXpGained() * difference)));
+            float newValue = event.getRawXpGained() - (event.getRawXpGained() * difference);
+
+            event.setRawXpGained(newValue);
+        }
+
+        mcMMOPlayer.getProfile().registeredXpGain(skillType, event.getRawXpGained());
     }
 }

Pershonkey/McMMOPlus

src/main/java/com/gmail/nossr50/listeners/SelfListener.java
@@ -6,10 +6,13 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.gmail.nossr50.config.Config;
+import com.gmail.nossr50.config.experience.ExperienceConfig;
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
 import com.gmail.nossr50.events.experience.McMMOPlayerXpGainEvent;
 import com.gmail.nossr50.events.skills.abilities.McMMOPlayerAbilityActivateEvent;
+import com.gmail.nossr50.util.player.UserManager;
 import com.gmail.nossr50.util.scoreboards.ScoreboardManager;
 
 public class SelfListener implements Listener {
@@ -37,5 +40,36 @@ public class SelfListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onAbility(McMMOPlayerAbilityActivateEvent event) {
         ScoreboardManager.cooldownUpdate(event.getPlayer(), event.getSkill());
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerXpGain(McMMOPlayerXpGainEvent event) {
+        int threshold = ExperienceConfig.getInstance().getDeminishedReturnsThreshold();
+
+        if (threshold <= 0) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        McMMOPlayer mcMMOPlayer = UserManager.getPlayer(player);
+        SkillType skillType = event.getSkill();
+
+        if (skillType.isChildSkill()) {
+            return;
+        }
+
+        float difference = (mcMMOPlayer.getProfile().getRegisteredXpGain(skillType) - threshold) / threshold;
+
+        if (difference > 0) {
+//            System.out.println("Total XP Earned: " + mcMMOPlayer.getProfile().getRegisteredXpGain(skillType) + " / Threshold value: " + threshold);
+//            System.out.println(difference * 100 + "% over the threshold!");
+//            System.out.println("Previous: " + event.getRawXpGained());
+//            System.out.println("Adjusted XP " + (event.getRawXpGained() - (event.getRawXpGained() * difference)));
+            float newValue = event.getRawXpGained() - (event.getRawXpGained() * difference);
+
+            event.setRawXpGained(newValue);
+        }
+
+        mcMMOPlayer.getProfile().registeredXpGain(skillType, event.getRawXpGained());
     }
 }